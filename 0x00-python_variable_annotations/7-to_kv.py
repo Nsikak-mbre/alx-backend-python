eccfa2323b20f7b94beebc90019ef29687f3df71
Nsikak-mbre <tm260715@gmail.com>
2024-12-23 12:58:03 +0100

    7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 7. Complex types - string and int/float to tuple """
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Returns a tuple with the string and the square of the number """
+    return (k, v * v)
