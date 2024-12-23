999cdb63a41f3922a524a6b51bed4177d0eb217d
Nsikak-mbre <tm260715@gmail.com>
2024-12-23 12:13:24 +0100

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Sum of a list of integers."""
+from typing import List
+
+
+def sum_list(input_list: List[int]):
+    """Sum of a list of integers."""
+    return sum(input_list)
