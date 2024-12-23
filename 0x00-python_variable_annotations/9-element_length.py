47fab9af182381531a36d633df796a598806b2c8
Nsikak-mbre <tm260715@gmail.com>
2024-12-23 16:04:57 +0100

    9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function element_length that takes a list of strings"""
 from typing import Iterable, Sequence, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> Iterable[Tuple[Sequence, int]]:
+    """Type-annotated function element_length that takes a list of strings"""
     return [(i, len(i)) for i in lst]
