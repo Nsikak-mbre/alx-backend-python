5b6cbf3972d56248dcaea8ae56865390662b6b79
Nsikak-mbre <tm260715@gmail.com>
2024-12-23 10:44:06 +0100

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floats and return the result.
+
+    Args:
+        a (float): The first float to add.
+        b (float): The second float to add.
+
+    Returns:
+        float: The sum of the two floats.
+    """
+    return a + b
